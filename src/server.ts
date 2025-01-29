1c9d56955068c22e87ea1758fc4fb8162a61e50f
LinCie <aldiofernanda@protonmail.com>
2025-01-29 17:05:17 +0700

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { logger } from "./utils";
+import morgan from "morgan";
+import helmet from "helmet";
+
+const app = express();
+const port = Bun.env.PORT || 8080;
+
+app
+
+	/**
+	 * Middlewares
+	 */
+
+	// Morgan
+	.use(
+		morgan("tiny", {
+			stream: {
+				write: (message) => logger.info(message.trim()),
+			},
+		}),
+	)
+
+	// Helmet
+	.use(helmet())
+
+	/**
+	 * Routes
+	 */
+	.get("/", (req, res) => {
+		res.send("Hello World!");
+	})
+
+	/**
+	 * Listen command
+	 */
+	.listen(port, () => {
+		logger.info(`Listening on port ${port}...`);
+	});
