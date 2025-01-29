4f080fd503a771ef4113b6778d205a6c0a77b780
LinCie <aldiofernanda@protonmail.com>
2025-01-29 22:02:37 +0700

    chore: add build command and pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+export const apps = [
+	{
+		name: "nerpai",
+		cwd: "./dist",
+		script: "./server",
+		exec_mode: "fork",
+		instances: 1,
+		autorestart: true,
+		watch: false,
+		max_memory_restart: "1024M",
+	},
+];
